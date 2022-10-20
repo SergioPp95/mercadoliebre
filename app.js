89c6685809ad8de4779cc98ce753ffa827acb995
SergioPp95 <sergiopepe.it@gmail.com>
2022-10-20 14:17:39 -0300

    mercadoliebre

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 
 const publicPath = path.join(__dirname, "./public")
 app.use( express.static( publicPath ) );
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"))
 })
